d755a82118ef588c1c0845ce79ec372e91c2d901
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-10 22:10:01 +0200

    spotless

--- src/main/java/mcp/mobius/waila/overlay/OverlayConfig.java
+++ src/main/java/mcp/mobius/waila/overlay/OverlayConfig.java
@@ -5,7 +5,6 @@ import net.minecraftforge.common.config.Configuration;
 import mcp.mobius.waila.api.impl.ConfigHandler;
 import mcp.mobius.waila.utils.Constants;
 
-// spotless:off
 public class OverlayConfig {
 
     public static int posX;
@@ -17,6 +16,7 @@ public class OverlayConfig {
     public static int fontcolor;
     public static float scale;
 
+    // spotless:off
     public static void updateColors() {
         alpha     =  (int) (ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_ALPHA, 0) / 100.0f * 256) << 24;
         bgcolor   = alpha + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_BGCOLOR, 0);
@@ -24,5 +24,5 @@ public class OverlayConfig {
         gradient2 = alpha + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_GRADIENT2, 0);
         fontcolor =         ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_FONTCOLOR, 0);
     }
+    // spotless:on
 }
-// spotless:on
