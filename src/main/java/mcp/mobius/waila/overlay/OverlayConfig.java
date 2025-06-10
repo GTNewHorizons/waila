b923c080669a12b6ec758fe8501a0ca53c777889
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-10 21:57:10 +0200

    make strings rendering ignore transparency

--- src/main/java/mcp/mobius/waila/overlay/OverlayConfig.java
+++ src/main/java/mcp/mobius/waila/overlay/OverlayConfig.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.config.Configuration;
 import mcp.mobius.waila.api.impl.ConfigHandler;
 import mcp.mobius.waila.utils.Constants;
 
+// spotless:off
 public class OverlayConfig {
 
     public static int posX;
@@ -17,16 +18,11 @@ public class OverlayConfig {
     public static float scale;
 
     public static void updateColors() {
-        OverlayConfig.alpha = (int) (ConfigHandler.instance()
-                .getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_ALPHA, 0) / 100.0f
-                * 256) << 24;
-        OverlayConfig.bgcolor = OverlayConfig.alpha
-                + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_BGCOLOR, 0);
-        OverlayConfig.gradient1 = OverlayConfig.alpha
-                + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_GRADIENT1, 0);
-        OverlayConfig.gradient2 = OverlayConfig.alpha
-                + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_GRADIENT2, 0);
-        OverlayConfig.fontcolor = OverlayConfig.alpha
-                + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_FONTCOLOR, 0);
+        alpha     =  (int) (ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_ALPHA, 0) / 100.0f * 256) << 24;
+        bgcolor   = alpha + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_BGCOLOR, 0);
+        gradient1 = alpha + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_GRADIENT1, 0);
+        gradient2 = alpha + ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_GRADIENT2, 0);
+        fontcolor =         ConfigHandler.instance().getConfig(Configuration.CATEGORY_GENERAL, Constants.CFG_WAILA_FONTCOLOR, 0);
     }
 }
+// spotless:on
