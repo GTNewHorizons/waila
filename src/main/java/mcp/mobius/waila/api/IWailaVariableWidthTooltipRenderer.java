07b9d1bf921cb0e08d3c89603d2332671ba4cfe8
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-10-21 00:47:51 +0300

    Add documentation for IWailaVariableWidthTooltipRenderer (#44)

--- src/main/java/mcp/mobius/waila/api/IWailaVariableWidthTooltipRenderer.java
+++ src/main/java/mcp/mobius/waila/api/IWailaVariableWidthTooltipRenderer.java
@@ -1,9 +1,29 @@
 package mcp.mobius.waila.api;
 
+/**
+ * Interface for tooltip renderers that adjust their width depending on the width of the rest of the tooltip.
+ * setMaxLineWidth is called by {@link mcp.mobius.waila.overlay.Tooltip} before every tooltip render to inform the
+ * renderer of the width of the longest line in the tooltip.
+ */
 public interface IWailaVariableWidthTooltipRenderer extends IWailaTooltipRenderer {
 
-    void setMaxStringW(int width);
+    /**
+     * Informs the renderer of what the length of the longest line is.</br>
+     * Called by {@link mcp.mobius.waila.overlay.Tooltip} before every tooltip render.</br>
+     *
+     * @param width The width of the longest line in the tooltip.
+     *
+     */
+    void setMaxLineWidth(int width);
 
-    int getMaxStringW();
+    /**
+     * Returns the width of the longest line in the current tooltip.</br>
+     * With icons disabled the tooltip's background box is always 2 pixels wider to the right of the line than it is to
+     * the left.</br>
+     *
+     * @return The width of the longest line in the tooltip
+     *
+     */
+    int getMaxLineWidth();
 
 }
