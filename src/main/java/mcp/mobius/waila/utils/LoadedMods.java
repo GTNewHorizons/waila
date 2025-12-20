adcde8bcabb8ca05f9d48bc8c7ca31b1e2c4bcf5
Connor Colenso <connorc@blackmagicdesign.com>
2025-12-20 01:31:04 +0000

    Use formatNumber from GTNHLib

--- src/main/java/mcp/mobius/waila/utils/LoadedMods.java
+++ src/main/java/mcp/mobius/waila/utils/LoadedMods.java
@@ -7,5 +7,6 @@ public class LoadedMods {
     public static boolean FORGE_MULTIPART = Loader.isModLoaded("ForgeMultipart");
     public static boolean NEI = Loader.isModLoaded("NotEnoughItems");
     public static boolean GT5U = Loader.isModLoaded("gregtech_nh");
+    public static boolean GTNH_LIB = Loader.isModLoaded("gtnhlib");
     public static boolean WAILA_PLUGINS = Loader.isModLoaded("wailaplugins");
 }
