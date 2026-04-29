9c14533da100bc77f08bbbb20c3dd6aca70ad982
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-04-29 17:14:26 +0200

    Styling - Fixed rendering of the buttons - Added shadow to the text inside the buttons

--- src/main/java/mcp/mobius/waila/gui/widgets/LabelFixedFont.java
+++ src/main/java/mcp/mobius/waila/gui/widgets/LabelFixedFont.java
@@ -10,17 +10,20 @@ public class LabelFixedFont extends WidgetBase {
 
     protected String text = "";
     protected int color;
+    protected boolean shadow;
 
     public LabelFixedFont(IWidget parent, String text) {
         super(parent);
         this.setText(text);
         this.color = 0xFFFFFF;
+        this.shadow = false;
     }
 
     public LabelFixedFont(IWidget parent, String text, int color) {
         super(parent);
         this.setText(text);
         this.color = color;
+        this.shadow = false;
     }
 
     @Override
@@ -37,6 +40,10 @@ public class LabelFixedFont extends WidgetBase {
 
     public void setColor(int color) {
         this.color = color;
+    }
+
+    public void setShadow(boolean shadow) {
+        this.shadow = shadow;
     }
 
     private void updateGeometry() {
@@ -56,7 +63,8 @@ public class LabelFixedFont extends WidgetBase {
     @Override
     public void draw(Point pos) {
         this.saveGLState();
-        this.mc.fontRenderer.drawString(this.text, pos.getX(), pos.getY(), this.color);
+        if (this.shadow) this.mc.fontRenderer.drawStringWithShadow(this.text, pos.getX(), pos.getY(), this.color);
+        else this.mc.fontRenderer.drawString(this.text, pos.getX(), pos.getY(), this.color);
         this.loadGLState();
     }
 }
